f9fa30ef6bd2974f69cc8b910343a8b61cad592a
Benjamin Nørgaard <b@noergaard.dk>
2017-06-29 14:46:45 +0200

    Improve time unmarshalling

--- types.go
+++ types.go
@@ -13,6 +13,10 @@ func (et *ecoTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	var timeStr string
 	ecoLayout := "2006-01-02T15:04:05"
 	d.DecodeElement(&timeStr, &start)
+	if timeStr == "" {
+		et.T = time.Time{}
+		return nil
+	}
 	t, err := time.Parse(ecoLayout, timeStr)
 	if err != nil {
 		return err
